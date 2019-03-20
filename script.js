d4e060f756ffe0c9f58d5deb246269faf8d3df74
aceKiled <48761378+aceKiled@users.noreply.github.com>
2019-03-20 10:26:25 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("#hide").click(function(){
+ $(".hide").toggle();
+});
+
+
+
+//Spell #2
+
+$("#fade").click(function(){
+$(".fade").toggle();
+});
+
+
+
+//Spell #3
+
+$("#dead").click(function(){
+ $(".dead").toggle();
+});
+
+
+
